3afbf326d989d5d3440e03cc5b309bec7e9c52f6
emilyjreed <emilykjackson@gmail.com>
2018-06-05 17:39:31 -0400

    heroku deployed

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,6 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: process.env.DBUSER,
     password: process.env.DBPW,
     database: "burgers_db"
