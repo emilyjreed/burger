4a8ce0b75f170e3b3777ae1bc4dcb8a2246c81ba
emilyjreed <emilykjackson@gmail.com>
2018-06-05 10:42:15 -0400

    schema and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Frito Chilli Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Tzatziki Feta Lamb Burger');
